99583f7c3fbc73bf6e5325d63240330ab5423453
KhunMarana <khunaungpaing.it.tumlm@gmail.com>
2020-12-02 14:44:18 +0630

    first commit

---
+++ src/com/khunaungpaing/ass3/Ass3.java
@@ -0,0 +1,18 @@
+package com.khunaungpaing.ass3;
+
+public class Ass3 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 5; i++) {
+			int num = 1;
+			
+			for (int j = 0; j <= i; j++) {
+				System.out.print(num);
+				num++;
+			}
+			System.out.println();
+		}
+		
+	}
+}
